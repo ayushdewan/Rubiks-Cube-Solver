fdb93618d69cdcd54f29ad84269f9e5310fd803d
Ayush Dewan <ayushdewan02@gmail.com>
2018-04-01 22:02:51 -0700

    density tests added

---
+++ colorlabeler.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+bounds = {
+	"red" : (np.array([160, 75, 75]), np.array([180, 255, 255])),
+	"blue" : (np.array([110, 75, 75]), np.array([130, 255, 255])),
+	"green" : (np.array([40, 0, 0]), np.array([80, 255, 255])),
+	"yellow" : (np.array([20, 75, 75]), np.array([40, 255, 255])),
+	"white" : (np.array([0, 0, 50]), np.array([180, 20, 255])),
+	"orange" : (np.array([10, 100, 100]), np.array([10, 255, 255]))
+}
+
+def density(img, color):
+	lower = bounds[color][0]
+	upper = bounds[color][1]
+	mask = cv2.inRange(img, lower, upper)
+	return np.sum(mask)/255
--- test.py
+++ test.py
@@ -2,45 +2,45 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+from colorlabeler import density
 
 def screen_record():
     last_time = time.time()
     cv2.startWindowThread()
     # cv2.namedWindow("preview")
+
+    cap = cv2.VideoCapture(0)
+
     while(True):
-        # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,30,715,560)))
-        print('loop took {} seconds'.format(time.time()-last_time))
+        _, img = cap.read()
         last_time = time.time()
-        img = cv2.flip(cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB), 1)
-        img_hsv = cv2.cvtColor(printscreen, cv2.COLOR_BGR2HSV)
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
 
         offset = 75
         for i in (-1, 0, 1):
             for j in (-1, 0, 1):
                 cv2.circle(img,(358 + i * offset, 280 + j * offset), 5, (0,255,0), -1)
         """
-        red = [110, 50, 50], [130, 255, 255] BGR
-        blue = [0, 50, 50], [10, 255, 255] BGR
-        green = [45, 50, 50], [75, 255, 255] BGR
-        yellow = [20, 100, 100], [30, 255, 255] RGB
-        orange = [5, 100, 100], [15, 255, 255] RGB
-        white = [0, 0, 0], [180, 30, 255] RGB
+        red = [160, 75, 75], [180, 255, 255]
+        blue = [110, 75, 75], [130, 255, 255]
+        green = [40, 0, 0], [80, 255, 255]
+        yellow = [20, 75, 75], [40, 255, 255]
+        orange = [10, 100, 100], [20, 255, 255]
+        white = [0, 0, 50], [180, 20, 255]
         """
-        lower = np.array([0, 50, 50], dtype = "uint8")
-        upper = np.array([10, 255, 255], dtype = "uint8")
+        lower = np.array([35, 0, 0])
+        upper = np.array([80, 255, 255])
 
         mask = cv2.inRange(img_hsv, lower, upper)
-        output = cv2.bitwise_and(img_hsv, img_hsv, mask = mask)
+        output = cv2.bitwise_and(img, img, mask = mask)
 
-        detector = cv2.SimpleBlobDetector_create()
-        keypoints = detector.detect(img)
-        im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-
-        cv2.imshow('window', im_with_keypoints)
+        cv2.imshow('window', img)
+        print(density(img_hsv[403:463, 250:310], "yellow"))
         if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.imwrite("yert.jpg", img[478:538, 250:310])
             cv2.destroyAllWindows()
+            cap.release()
             break
 
 screen_record()
